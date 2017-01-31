c88d235acf0cf7c5b727befd6916a3e8c3f4fef8
Oscar Alberto Paredes Garduno <oscar.paredes@infotec.mx>
2017-01-30 19:26:13 -0600

    Agregar componente lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import {PeliculaService} from '../providers/peliculas'
+import {PeliculaService} from '../providers/pelicula'
 
 @NgModule({
   declarations: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { PeliculaService, IPelicula } from '../../providers/pelicula'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  peliculas: Array<IPelicula>;
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public peliculaService: PeliculaService) {
+    this.peliculas = peliculaService.obtenerPeliculas();
   }
+  seleccionarPelicula(pelicula: IPelicula) {
 
+  }
 }
